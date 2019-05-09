ba322be2f693908b9b5df9ec39e7e98ff4ef4754
SVETO <svetostoj@hotmail.com>
2019-05-09 10:51:04 +1000

    End lesson 270 next Using React protrals

---
+++ src/components/streams/StreamEdit.1.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+// import { fetchStream } from '../../actions';
+
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        // this.props.fetchStream(this.props.match.params.id)
+    }
+    render() {
+        console.log(this.props)
+        if (!this.props.stream){
+            return <div>... Loading </div>
+        } 
+
+        return <div>{this.props.stream.title}</div>;
+        
+        
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    //   console.log(ownProps)
+    return { sream: state.streams[ownProps.match.params.id] };
+};
+// , { fetchStream }
+export default connect(mapStateToProps)(StreamEdit);
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,27 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-// import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
 
     componentDidMount() {
-        // this.props.fetchStream(this.props.match.params.id)
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues)
     }
     render() {
-        console.log(this.props)
-        if (!this.props.stream){
+        if (!this.props.stream) {
             return <div>... Loading </div>
-        } 
-
-        return <div>{this.props.stream.title}</div>;
-        
-        
+        }
+        return (
+            <div>
+                <h3>Edit Stream</h3>
+                <StreamForm
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                    onSubmit={this.onSubmit} />
+            </div>
+        );
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    //   console.log(ownProps)
-    return { sream: state.streams[ownProps.match.params.id] };
+    // debugger;
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
 };
-// , { fetchStream }
-export default connect(mapStateToProps)(StreamEdit);
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
